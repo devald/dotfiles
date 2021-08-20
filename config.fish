9177cb4c79da59d8b0d86f9a36c3889834306988
Dévald Tari <devald.tari@gmail.com>
2021-08-20 18:01:44 +0200

    Update fish

--- config.fish
+++ config.fish
@@ -1,37 +1,18 @@
-set -gx VISUAL nvim
+set fish_greeting
+set -gx EDITOR nvim
+set -gx VISUAL $EDITOR
 set -gx GOPATH (go env GOPATH)
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'
 set -gx BAT_THEME base16
+set -gx TF_PLUGIN_CACHE_DIR "$HOME/.terraform.d"
 
 alias v    nvim
 alias vi   nvim
 alias g    git
 alias cat  bat
-alias bubu "brew update; and brew upgrade; and brew cask upgrade; and brew cleanup; and brew doctor"
-
-bind \cd "exit"
-
-function exit -d "Detach from the last tmux session or exit"
-  if test -n "$VIM"
-    builtin exit
-    return
-  else if test -z "$TMUX"
-    builtin exit
-    return
-  end
-
-  set -l panes (tmux list-panes | wc -l)
-  set -l windows (tmux list-windows | wc -l)
-  set -l count (math "$panes + $windows - 1")
-
-  if test $count -eq 1
-    command tmux detach
-  else
-    builtin exit
-  end
-end
+alias bubu "brew update; and brew upgrade; and brew cleanup; and brew doctor"
 
 function tmux -d "Attach or start a new tmux session"
   if test -z $argv[1]
@@ -41,19 +22,7 @@ function tmux -d "Attach or start a new tmux session"
   end
 end
 
-function op
-  if not command op list items > /dev/null 2>&1
-    eval (command op signin my)
-  end
-
-  command op $argv
-end
-
 if status --is-interactive
   set BASE16_SHELL "$HOME/.config/base16-shell/"
   source "$BASE16_SHELL/profile_helper.fish"
-  base16-dracula
 end
-
-eval (starship init fish)
-eval (direnv hook fish)

e2a633d3864ae8c3be79839a0d568f10937ee3a3
Dévald Tari <tari@devald.pro>
2024-10-08 22:29:13 +0200

    Change alacritty.nix font settings

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -7,8 +7,9 @@
       settings = {
         shell.program = "${pkgs.zsh}/bin/zsh";
         shell.args = [ "-l" ];
-        font.size = 14.0;
-        font.normal.family = "Fira Code";
+        font.size = 10.0;
+        font.normal.family = "FiraCode Nerd Font Mono";
+        font.normal.style = "Retina";
         selection.save_to_clipboard = true;
         cursor.style.shape = "Beam";
         mouse.hide_when_typing = true;
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./alacritty.nix
     ./git.nix
     ./sh.nix
     ./alacritty.nix

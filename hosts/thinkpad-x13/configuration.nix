2629eae7e2aee28404ebdf2ba784c8cb3dcf77df
Dévald Tari <tari@devald.pro>
2024-10-08 15:36:16 +0200

    Launch SDDM in Wayland to try to avoid running an X server.

--- hosts/thinkpad-x13/configuration.nix
+++ hosts/thinkpad-x13/configuration.nix
@@ -34,6 +34,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
